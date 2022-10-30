7a1b16727a924f99ab70a5792f2113b89ab4763a
Angelo <angelo.lop.alv@gmail.com>
2022-10-30 13:44:04 -0300

    first resource

---
+++ src/main/java/com/example/restspring/Greeting.java
@@ -0,0 +1,15 @@
+package com.example.restspring;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+   private long id;
+   private String content;
+
+}
